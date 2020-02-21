6f998480afc0f870c04c8baebeb512ed6a9d2562
JYKingWY <wyjyking@gmail.com>
2020-02-21 23:35:43 +0800

    配置静态资源，添加主页

---
+++ src/main/java/com/crayon/crayonnote/controller/PageController.java
@@ -0,0 +1,15 @@
+package com.crayon.crayonnote.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class PageController {
+
+    @RequestMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+}
--- src/main/java/com/crayon/crayonnote/controller/TestController.java
+++ src/main/java/com/crayon/crayonnote/controller/TestController.java
@@ -1,15 +1,21 @@
 package com.crayon.crayonnote.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/test")
 public class TestController {
 
     @RequestMapping("/test1")
     public String test1() {
         return "test1";
+    }
+
+
+    @RequestMapping("/index")
+    public String testIndex() {
+        return "home";
     }
 
 }

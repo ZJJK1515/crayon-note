33ae46052f419e5aea2a0a3b2e2c762b41b4a502
JYKingWY <wyjyking@gmail.com>
2020-02-21 21:19:15 +0800

    build project

---
+++ src/main/java/com/crayon/crayonnote/CrayonNoteApplication.java
@@ -0,0 +1,13 @@
+package com.crayon.crayonnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrayonNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrayonNoteApplication.class, args);
+    }
+
+}

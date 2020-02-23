520211c3ccee17385ead4af10fde0f16d3343b46
JYKingWY <wyjyking@gmail.com>
2020-02-23 17:33:24 +0800

    完成页面嵌套、切换功能

--- src/main/java/com/crayon/crayonnote/controller/PageController.java
+++ src/main/java/com/crayon/crayonnote/controller/PageController.java
@@ -1,7 +1,9 @@
 package com.crayon.crayonnote.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/")
@@ -16,6 +18,16 @@ public class PageController {
     @RequestMapping("/home")
     public String home() {
         return "home";
+    }
+
+    @GetMapping(value = "/getPage")
+    public String getPage(String uri) {
+        return uri;
+    }
+
+    @RequestMapping("/getFooter")
+    public String getFooter() {
+        return "system/usermanage";
     }
 
 }
--- src/main/java/com/crayon/crayonnote/system/service/impl/SysMenuServiceImpl.java
+++ src/main/java/com/crayon/crayonnote/system/service/impl/SysMenuServiceImpl.java
@@ -1,16 +1,17 @@
 package com.crayon.crayonnote.system.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.crayon.crayonnote.entity.SysMenu;
 import com.crayon.crayonnote.system.dao.SysMenuDao;
 import com.crayon.crayonnote.system.service.SysMenuService;
+import lombok.extern.java.Log;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Log
 public class SysMenuServiceImpl extends ServiceImpl<SysMenuDao, SysMenu> implements SysMenuService {
 
 
@@ -22,6 +23,7 @@ public class SysMenuServiceImpl extends ServiceImpl<SysMenuDao, SysMenu> impleme
             //查询子菜单
             menu.setChildMenuList(this.list(new QueryWrapper<SysMenu>().eq("parent_id", menu.getMenuId())));
         }
+        log.info(sysMenuList.toString());
         return sysMenuList;
     }
 }

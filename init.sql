08dab847bfc2b32320f4311bac17815599f53a4c
JYKingWY <wyjyking@gmail.com>
2020-08-26 22:54:11 +0800

    初始化脚本更新

--- init.sql
+++ init.sql
@@ -1,58 +1,143 @@
--- auto-generated definition
-create table sys_menu
-(
-    menu_id     int(20) auto_increment comment '菜单 ID'
-        primary key,
-    menu_name   varchar(20)                         not null,
-    uri         varchar(100)                        not null comment '菜单路径',
-    icon        varchar(20)                         null comment '菜单图标',
-    parent_id   int(20)                             not null comment '父菜单 ID',
-    create_time timestamp default CURRENT_TIMESTAMP not null comment '创建时间',
-    update_time timestamp default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间'
-);
+-- MySQL dump 10.13  Distrib 5.6.47, for Win64 (x86_64)
+--
+-- Host: localhost    Database: crayonnote
+-- ------------------------------------------------------
+-- Server version	5.6.47
 
-INSERT INTO crayonnote.sys_menu (menu_id, menu_name, uri, icon, parent_id, create_time, update_time) VALUES ('1231146873176068321', '首页', 'dashboard', 'dashboard', '0', '2020-02-22 19:19:17', '2020-02-22 19:19:36');
-INSERT INTO crayonnote.sys_menu (menu_id, menu_name, uri, icon, parent_id, create_time, update_time) VALUES ('1231146873177067522', '系统管理', '#', 'settings', '0', '2020-02-22 15:17:39', '2020-02-22 17:21:13');
-INSERT INTO crayonnote.sys_menu (menu_id, menu_name, uri, icon, parent_id, create_time, update_time) VALUES ('1231146877325234177', '用户管理', 'system/usermanage', 'person', '1231146873177067522', '2020-02-22 15:17:39', '2020-02-23 17:30:40');
-INSERT INTO crayonnote.sys_menu (menu_id, menu_name, uri, icon, parent_id, create_time, update_time) VALUES ('1231146879493689345', '博客管理', '#', 'settings', '0', '2020-02-22 15:42:52', '2020-02-22 17:21:13');
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `sys_code`
+--
 
--- auto-generated definition
-create table sys_user
-(
-    user_id  varchar(20) default '' not null comment '用户 ID'
-        primary key,
-    username varchar(20)            not null comment '用户名',
-    password varchar(50)            not null comment '密码',
-    salt     varchar(20)            null comment '盐值',
-    name     varchar(10)            not null comment '昵称',
-    email    varchar(50)            not null comment '邮箱',
-    create_time timestamp default CURRENT_TIMESTAMP not null comment '创建时间',
-    update_time timestamp default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间',
-    constraint sys_user_email_uindex
-        unique (email),
-    constraint sys_user_name_uindex
-        unique (name),
-    constraint sys_user_username_uindex
-        unique (username)
-)
-    comment '用户信息表';
+DROP TABLE IF EXISTS `sys_code`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sys_code` (
+  `code_id` varchar(20) NOT NULL DEFAULT '' COMMENT '码值id',
+  `code_type` varchar(20) DEFAULT NULL COMMENT '代码种类',
+  `code_name` varchar(20) NOT NULL COMMENT '代码名称',
+  `code_value` varchar(20) NOT NULL COMMENT '代码值',
+  `remark` varchar(20) NOT NULL DEFAULT '',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`code_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='系统码表';
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-INSERT INTO crayonnote.sys_user (user_id, username, password, salt, name, email) VALUES ('1', 'developer', 'b858e2c9d50970be2fda7d0949e4a980', 'UUID-UUID-UUID-UUID', 'developer', 'developer@gmail.com');
+--
+-- Dumping data for table `sys_code`
+--
 
+LOCK TABLES `sys_code` WRITE;
+/*!40000 ALTER TABLE `sys_code` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sys_code` ENABLE KEYS */;
+UNLOCK TABLES;
 
-drop table if exists sys_code;
--- auto-generated definition
-create table sys_code
-(
-    code_id     varchar(20) default ''                not null comment '码值id'
-        primary key,
-    code_type   varchar(20)                           null comment '代码种类',
-    code_name   varchar(20)                           not null comment '代码名称',
-    code_value  varchar(20)                           not null comment '代码值',
-    remark      varchar(20) default ''                not null,
-    create_time timestamp default CURRENT_TIMESTAMP not null comment '创建时间',
-    update_time timestamp default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间'
-)
-    comment '系统码表';
+--
+-- Table structure for table `sys_menu`
+--
 
+DROP TABLE IF EXISTS `sys_menu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sys_menu` (
+  `menu_id` varchar(20) NOT NULL COMMENT '菜单 ID',
+  `menu_name` varchar(20) NOT NULL,
+  `uri` varchar(100) NOT NULL COMMENT '菜单路径',
+  `icon` varchar(20) DEFAULT NULL COMMENT '菜单图标',
+  `parent_id` varchar(20) NOT NULL COMMENT '父菜单 ID',
+  `create_time` timestamp NOT NULL COMMENT '创建时间',
+  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`menu_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sys_menu`
+--
+
+LOCK TABLES `sys_menu` WRITE;
+/*!40000 ALTER TABLE `sys_menu` DISABLE KEYS */;
+INSERT INTO `sys_menu` VALUES ('1231146873176068321','首页','dashboard','dashboard','0','2020-02-22 11:19:17','2020-02-22 11:19:36'),('1231146873177067522','系统管理','#','settings','0','2020-02-22 07:17:39','2020-03-19 14:52:09'),('1231146877325234177','用户管理','system/usermanage','person','1231146873177067522','2020-02-22 07:17:39','2020-02-23 09:30:40'),('1231146879493689345','博客管理','#','settings','0','2020-02-22 07:42:52','2020-02-22 09:21:13'),('1231146879493689346','文章编辑','article/edit','edit','0','2020-02-22 07:42:52','2020-02-22 09:21:13');
+/*!40000 ALTER TABLE `sys_menu` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sys_user`
+--
+
+DROP TABLE IF EXISTS `sys_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sys_user` (
+  `user_id` varchar(20) NOT NULL DEFAULT '' COMMENT '用户 ID',
+  `username` varchar(20) NOT NULL COMMENT '用户名',
+  `password` varchar(50) NOT NULL COMMENT '密码',
+  `salt` varchar(20) DEFAULT NULL COMMENT '盐值',
+  `name` varchar(10) NOT NULL COMMENT '昵称',
+  `email` varchar(50) NOT NULL COMMENT '邮箱',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `flag` tinyint(4) NOT NULL DEFAULT '0',
+  `deleted` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `sys_user_email_uindex` (`email`),
+  UNIQUE KEY `sys_user_name_uindex` (`name`),
+  UNIQUE KEY `sys_user_username_uindex` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户信息表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sys_user`
+--
+
+LOCK TABLES `sys_user` WRITE;
+/*!40000 ALTER TABLE `sys_user` DISABLE KEYS */;
+INSERT INTO `sys_user` VALUES ('1','developer','b858e2c9d50970be2fda7d0949e4a980','UUID-UUID-UUID-UUID','developer','developer@gmail.com','2020-03-19 14:53:58','2020-03-19 14:54:22',0,0);
+/*!40000 ALTER TABLE `sys_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sys_user_token`
+--
+
+DROP TABLE IF EXISTS `sys_user_token`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sys_user_token` (
+  `user_id` varchar(20) NOT NULL COMMENT '用户 ID',
+  `token` varchar(50) NOT NULL COMMENT '用户 token',
+  `expired_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '过期时间',
+  `update_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '登录时间'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sys_user_token`
+--
+
+LOCK TABLES `sys_user_token` WRITE;
+/*!40000 ALTER TABLE `sys_user_token` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sys_user_token` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-08-26 22:53:37

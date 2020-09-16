03dc0e27a14cd20a7042e7a094a050a1fae55793
wyjyking <wyjyking@gmail.com>
2020-09-16 21:57:09 +0800

    引入VUE,修复上传配置问题

--- src/main/java/com/crayonnote/modules/article/controller/ArticleController.java
+++ src/main/java/com/crayonnote/modules/article/controller/ArticleController.java
@@ -1,7 +1,17 @@
 package com.crayonnote.modules.article.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/article")
 public class ArticleController {
+
+
+
+    @RequestMapping("/")
+    public void saveArticle() {
+
+    }
+
 }
--- src/main/java/com/crayonnote/modules/file/service/impl/FileServiceImpl.java
+++ src/main/java/com/crayonnote/modules/file/service/impl/FileServiceImpl.java
@@ -16,8 +16,17 @@ import java.io.IOException;
 @Service
 public class FileServiceImpl implements IFileService {
 
+    /**
+     * 外部访问静态资源的路径前缀
+     */
     @Value("${crayon-note.file-upload.file-url-path}")
     private String fileUrlPath;
+
+    /**
+     * 上传文件存储到服务器的路径
+     */
+    @Value("${crayon-note.file-upload.upload-path}")
+    private String uploadPath;
 
     /**
      * 文件上传接口，暂存到服务器上
@@ -27,7 +36,7 @@ public class FileServiceImpl implements IFileService {
      */
     @Override
     public String upload(MultipartFile uploadFile) {
-        File file = new File("/crayon_file/" + uploadFile.getOriginalFilename());
+        File file = new File(uploadPath + "/" + uploadFile.getOriginalFilename());
         try {
             uploadFile.transferTo(file);
         } catch (IOException e) {

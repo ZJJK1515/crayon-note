a6efd9e23624cb6e2a8567a13da80dc3e6cec3f7
wyjyking <wyjyking@gmail.com>
2020-12-02 23:17:01 +0800

    集成Eureka

--- src/main/java/com/crayonnote/CrayonNoteApplication.java
+++ src/main/java/com/crayonnote/CrayonNoteApplication.java
@@ -2,8 +2,10 @@ package com.crayonnote;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableEurekaClient
 @EnableTransactionManagement
 @SpringBootApplication
 public class CrayonNoteApplication {
